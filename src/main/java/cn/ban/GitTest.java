f8a31e7cc731a2094422436575d43fabc8a5ad48
banjipeng <ban_jipeng@163.com>
2021-11-30 22:15:37 +0800

    git commit git-test second

--- src/main/java/cn/ban/GitTest.java
+++ src/main/java/cn/ban/GitTest.java
@@ -5,5 +5,9 @@ public class GitTest {
     public static void main(String[] args) {
 
         System.out.println("hello git");
+        System.out.println("hello github");
+        System.out.println("hello gitlab");
+        System.out.println("hello gits");
+
     }
 }
